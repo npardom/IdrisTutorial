a18e5ae1387b2ec02cbb671f10ed6113fa647646
npardom <npardom@unal.edu.co>
2023-05-03 23:10:43 -0500

    parallax

--- src/App.jsx
+++ src/App.jsx
@@ -19,6 +19,7 @@ function App() {
   return (
     <>
     <div className ="masterContainer"></div>
+    <div className ="linesContainer"></div>
       <MainMenu/>
       <div className ="contentContainer" id ="contentContainerId">
         <Header />
