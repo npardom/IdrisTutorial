ceb508768f739825a8114756dd1337a94b708c23
Jrativa <jrativa@unal.edu.co>
2023-05-07 15:47:31 -0500

    first info updated

--- src/components/FirstInfo.jsx
+++ src/components/FirstInfo.jsx
@@ -3,7 +3,7 @@ function FirstInfo() {
     <div className = "firstInfo">
         <hr></hr>
         <p>
-            <b>Idris</b> es un lenguaje de programación puramente funcional <b>con tipos dependientes</b>, lanzado en el año 2007.
+            <b>Idris</b> es un lenguaje de programación puramente funcional <b>con tipos dependientes</b> , evaluación diferida opcional y características como un verificador de totalidad . <b>Idris</b> se puede usar como asistente de prueba , pero está diseñado para ser un lenguaje de programación de propósito general similar a Haskell. Fue lanzado en el año 2007.
         </p>
         <hr></hr>
     </div>
